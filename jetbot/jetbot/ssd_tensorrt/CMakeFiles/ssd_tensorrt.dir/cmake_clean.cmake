a130e8c5145b5951beebda18c802b561200a014c
Ariel Dobrovensky <dynamodex@gmail.com>
2023-04-22 11:35:36 -0700

    fix

---
+++ jetbot/jetbot/ssd_tensorrt/CMakeFiles/ssd_tensorrt.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/ssd_tensorrt.dir/FlattenConcat.cpp.o"
+  "libssd_tensorrt.pdb"
+  "libssd_tensorrt.so"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/ssd_tensorrt.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
