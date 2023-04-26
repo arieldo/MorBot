e9a4f7aebbf086efcaeede62089b1702b3312541
Ariel Dobrovenski <dynamodex@gmail.com>
2023-04-26 12:42:52 +0300

    add resources for workshop

---
+++ resources/ROS/jetbot_control_node/scripts/jetbot_controller.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python3
+
+import rospy
+from geometry_msgs.msg import Twist
+from jetbot import Robot
+
+print("jetbot controller")
+class JetBotController:
+    def __init__(self):
+        rospy.init_node('jetbot_controller', anonymous=True)
+
+        self.robot = Robot()
+
+        rospy.Subscriber('/cmd_vel', Twist, self.cmd_vel_callback)
+        rospy.spin()
+
+    def cmd_vel_callback(self, msg):
+        linear_x = msg.linear.x
+        angular_z = msg.angular.z
+
+        left_motor_speed = linear_x - angular_z
+        right_motor_speed = linear_x + angular_z
+
+        # Set motor speeds
+        self.robot.set_motors(left_motor_speed, right_motor_speed)
+
+if __name__ == '__main__':
+    try:
+        jetbot_controller = JetBotController()
+    except rospy.ROSInterruptException:
+        pass
